b9c6a8aaa7ee4dbd74e721d5709649e50866b551
Laura Paakkinen <la.paakkinen@gmail.com>
2020-09-02 21:11:52 +0200

    Docker and a few suggestions
    to get the app running on other people computers as well

--- src/main/java/micros/Latex.java
+++ src/main/java/micros/Latex.java
@@ -15,7 +15,7 @@ public class Latex {
     public static void renderLatex(String trescZadania) throws IOException {
 
         TeXFormula formula = new TeXFormula(trescZadania);
-        formula.createPNG(TeXConstants.STYLE_DISPLAY, 20, "/Users/soniaorlikowska/IdeaProjects/Micros/src/main/resources/Problem"+ System.currentTimeMillis() + ".png", Color.white, Color.black);
+        formula.createPNG(TeXConstants.STYLE_DISPLAY, 20, "./src/main/resources/Problem"+ System.currentTimeMillis() + ".png", Color.white, Color.black);
 
     }
 
